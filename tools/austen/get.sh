ac946ff49daed05e87213424c0f58c150df665b2
Keigh Rim <keigh.rim@gmail.com>
2019-01-11 14:26:51 -0500

    added auten letter to tool_conf, austen tool now outputs lif

--- tools/austen/get.sh
+++ tools/austen/get.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-wget -qO $2 https://lapps-clarin.github.io/sample-data/austen-letter-1796-$1.txt
+wget -qO $2 https://raw.githubusercontent.com/lapps-clarin/lapps-clarin.github.io/master/sample-data/austen-letter-1796-$1.txt.lif
